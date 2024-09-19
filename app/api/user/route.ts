d7c5f81f23bfd1e6fec25d140c276493bbdae448
Jadyn Worthington <jadynwor@udel.edu>
2024-09-19 19:51:20 -0400

    adding sign in and up

---
+++ app/api/user/route.ts
@@ -0,0 +1,63 @@
+import { db } from "@/app/lib/db";
+import { NextResponse } from "next/server";
+import { hash } from 'bcrypt';
+import * as z from 'zod'
+
+// Define schema for input validate
+const userSchema = z
+  .object({
+    username: z.string().min(1, 'Username is required').max(100),
+    email: z.string().min(1, 'Email is required').email('Invalid email'),
+    password: z
+      .string()
+      .min(1, 'Password is required')
+      .min(5, 'Password must have than 8 characters'),
+  })
+
+
+export async function POST(req: Request){
+    try{
+        const body = await req.json();
+        const {email,username,password} = userSchema.parse(body);
+
+        // Check if email already exist
+        const existingUserByEmail = await db.user.findUnique({
+            where: {email : email}
+        });
+        if(existingUserByEmail){
+            return NextResponse.json({user:null,message:"user with email already exist"}, {status:409})
+        }
+
+        // Check if user name already exist
+        const existingUserByUsername = await db.user.findUnique({
+            where: {username : username}
+        });
+        if(existingUserByUsername){
+            return NextResponse.json({user:null,message:"user with username already exist"}, {status:409})
+        }
+
+        //Store data in db
+        const hashPassword = await hash(password,5); //default 5 characters
+        const newUser = await db.user.create({
+            data: {
+                username,
+                email,
+                password: hashPassword,
+            }    
+        })
+
+        const { password: newUserPassword, ...rest} = newUser;
+
+
+
+
+
+        return NextResponse.json({user: newUser, message: "User Created successfully"}, {status:201});
+
+        return NextResponse.json({message: "Something went wrong"}, {status:500});
+
+
+    }catch(error){
+
+    }
+}
