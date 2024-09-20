8a5235d7ee485bcc4449fe2a829c8dd39546505c
CoreyWithTheSkills <dignifiedemail@gmail.com>
2024-09-19 20:25:49 -0400

    footer with no style

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,16 @@
 // components/header.tsx
 export default function Footer() {
     return (
-     <a href="">TestingLink</a>
+      <div className="footer-content">
+      <p>&copy; {new Date().getFullYear()} Budget Mix Corporation. All rights reserved.</p>
+      <nav>
+          <ul className="footer-links">
+              <li><a href="/privacy">Privacy Policy</a></li>
+              <li><a href="/terms">Terms of Service</a></li>
+              <li><a href="/contact">Contact Us</a></li>
+          </ul>
+      </nav>
+  </div>
     );
   }
   
